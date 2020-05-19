ecab00b4a02335e386262de7f395b31ca123cfe9
Javier Gonzalez <ashabefunker@gmail.com>
2020-05-19 10:47:20 -0500

    Se consume la API

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ApiService } from './api.service';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'TiendaAngular';
+
+  constructor(public json: ApiService) {
+    this.json.getData('http://demo6292426.mockable.io/products').subscribe((res: any) => {
+      console.log(res);
+    })
+    
+  }
 }
